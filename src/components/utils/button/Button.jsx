3be97d5d29f8bcbcf9398d3102a61d5c2646e48a
salazenas <salazenas@gmail.com>
2019-06-20 19:18:01 -0300

    Button data definition

--- src/components/utils/button/Button.jsx
+++ src/components/utils/button/Button.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 
-export default props => (
-    <button id={props.id} className={`${props.id === "cifrado" ? "btn btn-light" : "btn btn-dark"}`} onClick={props.onClick}>
-        {props.value}
-    </button>
-)
+let color = "";
+
+export default props => {
+
+    switch (props.id) {
+        case "cifrado":
+            color = "light"
+            break;
+        case "decifrado":
+            color = "dark"
+            break;
+        case "enviar":
+            color = "success"
+            break;
+        default:
+            color = "light"
+            break;
+    }
+
+    return (
+        <button id={props.id} className={`btn btn-${color}`} onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
+}
